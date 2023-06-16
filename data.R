12aa9e52bd4ff93ad459af13737e6deaa5bebc3a
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2023-06-16 22:38:58 +0200

    YFT

---
+++ data.R
@@ -0,0 +1,18 @@
+# data.R - DESC
+# iotc_tuna_fads/data.R
+
+# Copyright (c) WUR, 2023.
+# Author: Iago MOSQUEIRA (WMR) <iago.mosqueira@wur.nl>
+#
+# Distributed under the terms of the EUPL-1.2
+
+
+library(ss3om)
+
+# --- SKJ
+
+skj <- readFLSss3('data/skj')
+
+om <- readFLomss3('data/skj')
+
+
